14cec80ce4a6b0ace7385b4ed4e29c955cd713bb
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-07-11 07:37:51 +0100

    Initial Commit

---
+++ Thriends/ThriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThriendsApp.swift
+//  Thriends
+//
+//  Created by Mac on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
